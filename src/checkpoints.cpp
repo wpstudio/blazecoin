90d7836f8a2733151df7db6b54b5dae8755c7f46
Benjamin Bradley <benjamin@wpstudio.com>
2014-06-14 09:08:05 -0400

    Added checkpoint for block 60413

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -30,6 +30,7 @@ namespace Checkpoints
             (  0,    uint256("0x5d871c1b6ea542c2bb8a3b3ac70028a591bbf81369e90c2446c1a2bbfb89459b"))
             ( 18500, uint256("0x6cfc4decf7c26c037c621190681f9b3f12912f8330b06c652e0c193340a23347"))
             ( 33000, uint256("0xcfd0eb628a7fee82accc4e2f183cdd1abd08afeedf2b1b030230836a4e74629e"))
+            ( 60413, uint256("0x99ea9310aac366b03161254d77ada05c9eb1392ca8ca370885b013f80d41f56e"))
 			;
 
     bool CheckBlock(int nHeight, const uint256& hash)
